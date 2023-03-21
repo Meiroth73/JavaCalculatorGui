5bda95cbe6918a89702559aa01bf55fc45774091
Meiroth73 <kotyk9921@gmail.com>
2023-03-21 15:35:14 +0100

    Function buttons
    Add function buttons: +, -, *, /, =, C

--- src/main/java/com/example/pro1/Controller.java
+++ src/main/java/com/example/pro1/Controller.java
@@ -4,9 +4,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class Controller {
+
+    //zmienne pomocnicze
     boolean isFirstNumber = true;
     double firstNumber = 0;
     double secondNumber = 0;
+    double reasult;
+    char operation;
 
     private void addNumber(int x) {
         if (isFirstNumber) {
@@ -14,6 +18,7 @@ public class Controller {
             firstNumber += x;
             reasultWindow.setText(Double.toString(firstNumber));
         } else {
+            reasultWindow.clear();
             secondNumber *= 10;
             secondNumber +=x;
             reasultWindow.setText(Double.toString(secondNumber));
@@ -63,5 +68,62 @@ public class Controller {
     private void clickNine() {
         addNumber(9);
     }
-}
 
+    //przyciski funkcyjne
+    @FXML
+    private void clickPlus() {
+        isFirstNumber = false;
+        reasultWindow.clear();
+        operation = '+';
+    }
+    @FXML
+    private void clickMinus() {
+        isFirstNumber = false;
+        reasultWindow.clear();
+        operation = '-';
+    }
+    @FXML
+    private void clickMultiplication() {
+        isFirstNumber = false;
+        reasultWindow.clear();
+        operation = '*';
+    }
+    @FXML
+    private void clickDivision() {
+        isFirstNumber = false;
+        reasultWindow.clear();
+        operation = '/';
+    }
+    @FXML
+    private void clear() {
+        reasultWindow.clear();
+        isFirstNumber = true;
+        firstNumber = 0;
+        secondNumber = 0;
+    }
+    @FXML
+    private void getResult() {
+        switch (operation) {
+            case '+':
+                reasult = firstNumber + secondNumber;
+                if ((reasult / 10) > 9) {
+                    reasultWindow.setText(Double.toString(reasult));
+                } else {
+                    reasultWindow.setText(Integer.toString((int)reasult));
+                }
+                break;
+            case '-':
+                reasult = firstNumber - secondNumber;
+                reasultWindow.setText(Double.toString(reasult));
+                break;
+            case  '*':
+                reasult = firstNumber * secondNumber;
+                reasultWindow.setText(Double.toString(reasult));
+                break;
+            case '/':
+                reasult = firstNumber / secondNumber;
+                reasultWindow.setText(Double.toString(reasult));
+                break;
+        }
+    }
+}
